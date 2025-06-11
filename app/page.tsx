67389ec25035b3f2961e154d73d64e7a449c9376
Nadya Rere <nadya.rere@gmail.com>
2025-06-11 11:38:55 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h1 className="text-2xl underline">Welcome to My SaaS App</h1>
+      <Button>Get Started</Button>
+    </>
+  );
+};
+
+export default Page;
